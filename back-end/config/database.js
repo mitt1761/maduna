5942cc9216bcec7f93e097da9d3fa149e89e1d94
mitt1761 <miftahulreski073@gmail.com>
2026-07-16 02:24:03 +0800

    Update database.js

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -1,18 +1,22 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',      
-    password: '',      
-    database: 'db_maduna'
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Gagal terhubung ke database:', err);
-    } else {
-        console.log('Berhasil terhubung ke database db_maduna!');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
     }
 });
 
-module.exports = db; // Diekspor agar bisa dipakai oleh controller nanti
+connection.connect((err) => {
+    if (err) {
+        console.error('Koneksi gagal:', err);
+    } else {
+        console.log('MySQL Aiven Connected');
+    }
+});
+
+module.exports = connection;
