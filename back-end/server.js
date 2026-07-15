f7206646c71bfc5414ada3098741b458e4a2241e
mitt1761 <miftahulreski073@gmail.com>
2026-07-16 00:25:14 +0800

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Panggil koneksi database dari folder config agar ikut menyala
 require('./config/database');
 
-//  rute /api/login dll akan dipasang di sini) ---
+app.get('/', (req, res) => {
+    res.send('MadunaHost API is running...');
+});
+
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server API berjalan di http://localhost:${PORT}`);
+    console.log(`Server API berjalan di port ${PORT}`);
 });
