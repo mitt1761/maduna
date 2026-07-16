4b1c5221f295eb864be72fc8c8b8211aada0549a
mitt1761 <miftahulreski073@gmail.com>
2026-07-16 10:47:21 +0800

    Refactor database connection to use connection pool

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -2,22 +2,26 @@ require('dotenv').config();
 
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     ssl: {
         rejectUnauthorized: false
     }
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
-        console.error('Gagal terhubung ke database:', err);
+        console.error("Gagal konek Aiven:", err);
     } else {
-        console.log('Berhasil terhubung ke database Aiven!');
+        console.log("Berhasil konek Aiven");
+        connection.release();
     }
 });
 
